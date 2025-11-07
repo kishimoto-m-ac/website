d91c7b8443f6c67718c089bc3ed4ec1416a21f86
kishimoto-m-ac <kishimoto.m.ac@m.titech.ac.jp>
2025-11-07 18:08:50 +0900

    fix: wrap App in LanguageProvider (main.tsx修正反映)

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+// LanguageProviderのimportを追加！
+import { LanguageProvider } from './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </React.StrictMode>
-)
+);
